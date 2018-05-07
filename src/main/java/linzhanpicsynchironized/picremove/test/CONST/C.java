78206ab4296c25bda7bafb652239ca9afb733099
hanpenghu <563909408@qq.com>
2018-05-07 18:28:16 +0800

    Initial commit

---
+++ src/main/java/linzhanpicsynchironized/picremove/test/CONST/C.java
@@ -0,0 +1,13 @@
+package linzhanpicsynchironized.picremove.test.CONST;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+
+import linzhanpicsynchironized.picremove.test.dao.javaMapper.A001TongYongMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class C {
+    @Autowired
+    public A001TongYongMapper a001TongYongMapper;
+}
